6498a60757ee4e6b73a9c38862e74965c0ed5b01
Calin <velniciuc.calin@icloud.com>
2023-10-24 22:49:21 +0200

    feat | exercise 1 done

---
+++ script.js
@@ -0,0 +1,9 @@
+---- Il Generatore di Tabelle di Moltiplicazione ----
+
+let num = prompt('Inserisci un numero da 1 a 10');
+let i = 1;
+
+while( i <= 10 ) {
+ alert( num + 'x' + i + '=' + (num * i ));
+ i++ ;
+}
