5f2db4ad6d235e29edeebf168e4536c516fe912e
Calin <velniciuc.calin@icloud.com>
2023-10-24 22:55:41 +0200

    refactor | exercises finished & organized

--- script.js
+++ script.js
@@ -1,94 +1,94 @@
----- Il Generatore di Tabelle di Moltiplicazione ----
+// ---- Il Generatore di Tabelle di Moltiplicazione ----
 
-let num = prompt('Inserisci un numero da 1 a 10');
-let i = 1;
+// let num = prompt('Inserisci un numero da 1 a 10');
+// let i = 1;
 
-while( i <= 10 ) {
- alert( num + 'x' + i + '=' + (num * i ));
- i++ ;
-}
-
-
-
----- Il Sostitutore di Parole ----
-
-let frase = prompt('Scrivi una frase');
-let parola1 = prompt('Inscerisci parola da sostituire');
-let parola2 = prompt('Inscerisci sostituo');
-frase = frase.replace(parola1, parola2);
-alert(frase); 
+// while( i <= 10 ) {
+//  alert( num + 'x' + i + '=' + (num * i ));
+//  i++ ;
+// }
 
 
 
----- Il Validatore di Siti Web ----
+// ---- Il Sostitutore di Parole ----
 
-let url = prompt('Inserisci un url');
-if((url.startsWith('http://' || 'https://')) && url.endsWith('.com')){
-    alert('url valido');
-} else{
-    alert('url non valido');
-}
-
-
-
----- La Sfida del Lancio del Dado ----
-
-let vittoriePc = 0;
-let vittorieG = 0;
-let gioco = prompt('Vuoi giocare a dadi?')
-
-while (gioco == 'si' || gioco == 'Si' || gioco == 'SI') {
-
-    alert('Lancia il dado');
-
-    let dadoPc = Math.ceil(Math.random() * 6);
-    let dadoG = Math.ceil(Math.random() * 6);
-
-    alert(' TU: ' + dadoG + '  PC: ' + dadoPc);
-
-    if (dadoG > dadoPc) {
-        alert('Hai Vinto');
-        vittorieG++;
-    }
-
-    else if (dadoG < dadoPc) {
-        alert('Hai Perso');
-        vittoriePc++;
-    }
-
-    else {
-        alert('Pareggio');
-    }
-
-    alert('Punteggio: TU = ' + vittorieG + '  PC = ' + vittoriePc);
-    gioco = prompt('Vuoi continuare a giocare?')
-}
+// let frase = prompt('Scrivi una frase');
+// let parola1 = prompt('Inscerisci parola da sostituire');
+// let parola2 = prompt('Inscerisci sostituo');
+// frase = frase.replace(parola1, parola2);
+// alert(frase); 
 
 
 
----- Il Gioco dell Indovinello ----
+// ---- Il Validatore di Siti Web ----
 
-let numMax = prompt('Scegli il numero massimo tra cui indovinare');
+// let url = prompt('Inserisci un url');
+// if((url.startsWith('http://' || 'https://')) && url.endsWith('.com')){
+//     alert('url valido');
+// } else{
+//     alert('url non valido');
+// }
 
-let numX = Math.floor((Math.random() * numMax) + 1);
 
-let numG = undefined;
 
-while (!(numG===numX)){
-    let numG = prompt('Indovina il numero al quale sto pensando');
+// ---- La Sfida del Lancio del Dado ----
 
-    if (numG > numX){
-        alert('Sbagliato! Prova con un numero più basso');
-    }
-    else if (numG < numX){
-        alert('Sbagliato! Prova con un numero più alto');
-    }
-    else {
-        alert('Congratulazioni! Hai Indovinato!');
-        break;
-    }
+// let vittoriePc = 0;
+// let vittorieG = 0;
+// let gioco = prompt('Vuoi giocare a dadi?')
 
-}
+// while (gioco == 'si' || gioco == 'Si' || gioco == 'SI') {
+
+//     alert('Lancia il dado');
+
+//     let dadoPc = Math.ceil(Math.random() * 6);
+//     let dadoG = Math.ceil(Math.random() * 6);
+
+//     alert(' TU: ' + dadoG + '  PC: ' + dadoPc);
+
+//     if (dadoG > dadoPc) {
+//         alert('Hai Vinto');
+//         vittorieG++;
+//     }
+
+//     else if (dadoG < dadoPc) {
+//         alert('Hai Perso');
+//         vittoriePc++;
+//     }
+
+//     else {
+//         alert('Pareggio');
+//     }
+
+//     alert('Punteggio: TU = ' + vittorieG + '  PC = ' + vittoriePc);
+//     gioco = prompt('Vuoi continuare a giocare?')
+// }
+
+
+
+// ---- Il Gioco dell Indovinello ----
+
+// let numMax = prompt('Scegli il numero massimo tra cui indovinare');
+
+// let numX = Math.floor((Math.random() * numMax) + 1);
+
+// let numG = undefined;
+
+// while (!(numG===numX)){
+//     let numG = prompt('Indovina il numero al quale sto pensando');
+
+//     if (numG > numX){
+//         alert('Sbagliato! Prova con un numero più basso');
+//     }
+//     else if (numG < numX){
+//         alert('Sbagliato! Prova con un numero più alto');
+//     }
+//     else {
+//         alert('Congratulazioni! Hai Indovinato!');
+//         break;
+//     }
+
+// }
 
 
 
